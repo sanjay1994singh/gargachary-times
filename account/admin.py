659282211036905d969fb529d3200a15f6b67667
sanjay <sanjaypython22@gmail.com>
2025-07-20 18:08:15 +0530

    p

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import User
+
 
 # Register your models here.
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['mobile', 'city', 'district', 'state']
+
+
+admin.site.register(User, UserAdmin)
