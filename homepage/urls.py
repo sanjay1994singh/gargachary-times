7dff6076535cc5b9716fd18748d3dea1a8392742
sanjay <sanjaypython22@gmail.com>
2025-07-28 10:09:47 +0530

    p

--- homepage/urls.py
+++ homepage/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.homepage, name='homepage'),
+    path('category_news/<int:id>/', views.category_news, name='category_news'),
 ]
--- homepage/views.py
+++ homepage/views.py
@@ -11,13 +11,32 @@ from category.models import Category
 
 # Create your views here.
 def homepage(request):
-    user_id = request.session.get('user_id')
-    breaking_video = Video.objects.all().order_by('-id')
-    news = News.objects.all().order_by('-id')[:7]
-    category = Category.objects.all().order_by('-id')
+    all_news = list(News.objects.all().order_by('-id')[:30])
+
+    # Split into 3 parts
+    column_2 = all_news[:10]  # latest 10
+    column_1 = all_news[10:20]  # next 10
+    column_3 = all_news[20:30]  # last 10
+
     context = {
-        'breaking_video': breaking_video,
-        'news': news,
-        'category': category,
+        'news_col1': column_1,
+        'news_col2': column_2,
+        'news_col3': column_3,
     }
     return render(request, 'index1.html', context)
+
+
+def category_news(request, id):
+    all_news = list(News.objects.filter(category_id=id).order_by('-id')[:30])
+
+    # Split into 3 parts
+    column_2 = all_news[:10]  # latest 10
+    column_1 = all_news[10:20]  # next 10
+    column_3 = all_news[20:30]  # last 10
+
+    context = {
+        'news_col1': column_1,
+        'news_col2': column_2,
+        'news_col3': column_3,
+    }
+    return render(request, 'category_news.html', context)
