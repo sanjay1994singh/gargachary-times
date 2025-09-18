88ca27ce942ccdf42809ce26d48708286176c1e5
sanjay <sanjaypython22@gmail.com>
2025-09-18 19:02:07 +0530

    p

--- news_pdf/views.py
+++ news_pdf/views.py
@@ -4,5 +4,14 @@ from .models import NewsPDF
 
 # Create your views here.
 def news_pdf(request):
-    latest_pdf = NewsPDF.objects.last()
+    selected_date = request.GET.get("date")
+
+    if selected_date:
+        # filter by date part of uploaded_at
+        latest_pdf = NewsPDF.objects.filter(
+            uploaded_at__date=selected_date
+        ).first()
+    else:
+        # fallback → latest PDF
+        latest_pdf = NewsPDF.objects.last()
     return render(request, 'news_pdf.html', {'pdf': latest_pdf})
