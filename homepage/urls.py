25807bcd359768cbf0754c50a48e11bbf1fa9bd5
sanjay <sanjaypython22@gmail.com>
2025-12-14 22:01:20 +0530

    p

--- homepage/urls.py
+++ homepage/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.homepage, name='homepage'),
-    path('/contact/', views.contact, name='contact'),
+    path('contact/', views.contact, name='contact'),
     path('video/', views.video, name='video'),
     path('category_news/<int:id>/', views.category_news, name='category_news'),
 ]
