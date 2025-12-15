c7aae063d68bd0c8c8a70e65b9cc3cc1772fcb2f
sanjay <sanjaypython22@gmail.com>
2025-12-15 10:28:47 +0530

    p

--- news_pdf/views.py
+++ news_pdf/views.py
@@ -5,23 +5,22 @@ from .models import NewsPDF
 # Create your views here.
 def news_pdf(request):
     selected_date = request.GET.get("date")
-
+    print(selected_date, '=================q')
     if selected_date:
         latest_pdf = NewsPDF.objects.filter(
             uploaded_at__date=selected_date
         ).first()
-        pdf = NewsPDF.objects.get(id=latest_pdf.id)
+        current_pdf = NewsPDF.objects.get(id=latest_pdf.id)
 
         absolute_image_url = request.build_absolute_uri(pdf.featured_image.url)
     else:
         latest_pdf = NewsPDF.objects.last()
-        pdf = NewsPDF.objects.get(id=latest_pdf.id)
-
+        current_pdf = NewsPDF.objects.get(id=latest_pdf.id)
         absolute_image_url = request.build_absolute_uri(pdf.featured_image.url)
 
     context = {
         'pdf': latest_pdf,
-        'news': pdf,
+        'news': current_pdf,
         'absolute_image_url': absolute_image_url,
     }
 
