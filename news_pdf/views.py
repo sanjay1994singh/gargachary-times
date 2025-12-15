d2f8d3e9d5ae5cd101226900a1148067c0aeff39
sanjay <sanjaypython22@gmail.com>
2025-12-15 10:20:48 +0530

    p

--- news_pdf/views.py
+++ news_pdf/views.py
@@ -10,14 +10,16 @@ def news_pdf(request):
         latest_pdf = NewsPDF.objects.filter(
             uploaded_at__date=selected_date
         ).first()
-        absolute_image_url = request.build_absolute_uri(latest_pdf.featured_image.url)
+        pdf = NewsPDF.objects.get(latest_pdf.id)
+        absolute_image_url = request.build_absolute_uri(pdf.featured_image.url)
     else:
         latest_pdf = NewsPDF.objects.last()
-        absolute_image_url = request.build_absolute_uri(latest_pdf.featured_image.url)
+        pdf = NewsPDF.objects.get(latest_pdf.id)
+        absolute_image_url = request.build_absolute_uri(pdf.featured_image.url)
 
     context = {
         'pdf': latest_pdf,
-        'news': latest_pdf,
+        'news': pdf,
         'absolute_image_url': absolute_image_url,
     }
 
