4b8b6302d2b35087d008842a870354d53e8fbf87
sanjay <sanjaypython22@gmail.com>
2025-12-17 23:35:15 +0530

    p

--- category/models.py
+++ category/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 # Create your models here.
 class Category(models.Model):
+    city = models.BooleanField(default=False)
     name = models.CharField(max_length=100, null=True, blank=True)
     desc = models.TextField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
