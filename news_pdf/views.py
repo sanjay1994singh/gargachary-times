e6cc96d341ce1fa52b4c4cdf61a26cf61bcea809
sanjay <sanjaypython22@gmail.com>
2025-12-18 22:06:02 +0530

    p

--- news_pdf/views.py
+++ news_pdf/views.py
@@ -3,23 +3,36 @@ from .models import NewsPDF
 
 
 # Create your views here.
-def news_pdf(request):
-    selected_date = request.GET.get("date")
-    if selected_date:
-        latest_pdf = NewsPDF.objects.filter(
-            uploaded_at__date=selected_date
-        ).first()
-        current_pdf = NewsPDF.objects.get(id=latest_pdf.id)
-    else:
-        latest_pdf = NewsPDF.objects.last()
-        current_pdf = NewsPDF.objects.get(id=latest_pdf.id)
+# def news_pdf(request):
+#     selected_date = request.GET.get("date")
+#     if selected_date:
+#         latest_pdf = NewsPDF.objects.filter(
+#             uploaded_at__date=selected_date
+#         ).first()
+#         current_pdf = NewsPDF.objects.get(id=latest_pdf.id)
+#
+#         absolute_image_url = request.build_absolute_uri(current_pdf.featured_image.url)
+#     else:
+#         latest_pdf = NewsPDF.objects.last()
+#         current_pdf = NewsPDF.objects.get(id=latest_pdf.id)
+#         absolute_image_url = request.build_absolute_uri(current_pdf.featured_image.url)
+#
+#     context = {
+#         'pdf': latest_pdf,
+#         'news': current_pdf,
+#         'absolute_image_url': absolute_image_url,
+#     }
+#
+#     return render(request, 'news_pdf.html', context)
 
+
+def news_pdf(request):
+    news = NewsPDF.objects.last()
     absolute_image_url = request.build_absolute_uri(current_pdf.featured_image.url)
 
     context = {
-        'news': current_pdf,
+        'news': news,
         'absolute_image_url': absolute_image_url,
     }
 
     return render(request, 'pdf.html', context)
-    return render(request, 'news_pdf.html', context)
