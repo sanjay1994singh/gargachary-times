dadc4247d72dd4b0560723a15a00b2057e2adfe9
sanjay1994singh <sanjaypython22@gmail.com>
2026-01-18 22:47:30 +0530

    p

--- news/context_processors.py
+++ news/context_processors.py
@@ -1,8 +1,29 @@
-# news/context_processors.py
+# # news/context_processors.py
+#
+# from category.models import Category, State
+#
+# def category_context(request):
+#     return {
+#         'category': Category.objects.all().order_by('-id'),
+#         'state': State.objects.all().order_by('-id')
+#     }
 
-from category.models import Category
+
+from category.models import Category, State
+from django.db.models import Prefetch
+
 
 def category_context(request):
+    states = State.objects.prefetch_related(
+        Prefetch(
+            'category_set',
+            queryset=Category.objects.filter(city=True).order_by('name')
+        )
+    ).order_by('name')
+
+    other_categories = Category.objects.filter(city=False).order_by('name')
+
     return {
-        'category': Category.objects.all().order_by('-id')
+        'states': states,
+        'other_categories': other_categories,
     }
