e8aebb5dcecf61938fd3ece1cb97e7e18ba150e2
sanjay1994singh <sanjaypython22@gmail.com>
2026-01-18 22:11:26 +0530

    p

--- category/admin.py
+++ category/admin.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
-from .models import Category
+from .models import Category, State
 
 
 # Register your models here.
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ['name', 'created_at', 'id']
 
+class StateAdmin(admin.ModelAdmin):
+    list_display = ['name', 'created_at', 'id']
 
+
+admin.site.register(State, StateAdmin)
 admin.site.register(Category, CategoryAdmin)
