d0bdaf123ff6ea6fe44145ce9ee80b3f484532a0
sanjay1994singh <sanjaypython22@gmail.com>
2026-01-18 22:04:33 +0530

    p

--- category/models.py
+++ category/models.py
@@ -2,7 +2,21 @@ from django.db import models
 
 
 # Create your models here.
+class State(models.Model):
+    name = models.CharField(max_length=100, null=True, blank=True)
+    desc = models.TextField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True, null=True, blank=True)
+
+    def __str__(self):
+        return str(self.name)
+
+    class Meta:
+        db_table = 'state'
+
+
 class Category(models.Model):
+    state = models.ForeignKey(State, on_delete=models.CASCADE, null=True)
     city = models.BooleanField(default=False)
     name = models.CharField(max_length=100, null=True, blank=True)
     desc = models.TextField(null=True, blank=True)
