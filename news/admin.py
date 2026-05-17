9351d510ecb7c4cb2dca34ceada7f9b02e722ccf
sanjay1994singh <sanjaypython22@gmail.com>
2026-05-17 14:08:36 +0530

    p

--- news/admin.py
+++ news/admin.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
-from .models import News
+from .models import News, Visitor
 
 
 # Register your models here.
 class NewsAdmin(admin.ModelAdmin):
     list_display = ['title', 'category', 'count', 'created_at', 'id']
 
+class VisitorAdmin(admin.ModelAdmin):
+    list_display = ['ip_address', 'city', 'state', 'country', 'id']
+
 
 admin.site.register(News, NewsAdmin)
+admin.site.register(Visitor, VisitorAdmin)
