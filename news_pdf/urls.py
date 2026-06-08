38177cbb4da16868eebcea73d3c4765f1f197b2e
sanjay1994singh <sanjaypython22@gmail.com>
2026-06-08 17:01:56 +0530

    p

--- news_pdf/urls.py
+++ news_pdf/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.news_pdf, name='news_pdf'),
     path('new_news_pdf/', views.new_news_pdf, name='new_news_pdf'),
     path('api/epaper/', views.epaper_list, name='epaper_list'),
 ]
