1c083513c5d111b0fd1423f398bdf7c8b4d814e6
sanjay1994singh <sanjaypython22@gmail.com>
2026-06-12 03:41:34 +0530

    p

--- news/signals.py
+++ news/signals.py
@@ -8,13 +8,13 @@ from .facebook import post_to_facebook
 @receiver(post_save, sender=News)
 def auto_post_news(sender, instance, created, **kwargs):
 
-    # if created:
-    try:
-        post_to_facebook(instance)
-        print("Facebook post successful")
-        instance.facebook_posted = True
-        News.objects.filter(pk=instance.pk).update(
-            facebook_posted=True
-        )
-    except Exception as e:
-        print("Facebook post failed:", e)
+    if created:
+        try:
+            post_to_facebook(instance)
+            print("Facebook post successful")
+            instance.facebook_posted = True
+            News.objects.filter(pk=instance.pk).update(
+                facebook_posted=True
+            )
+        except Exception as e:
+            print("Facebook post failed:", e)
