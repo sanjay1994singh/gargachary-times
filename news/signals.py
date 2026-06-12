b9cf4bf32c9454149fdfbbaec8c1006473ce8109
sanjay1994singh <sanjaypython22@gmail.com>
2026-06-12 12:01:23 +0530

    p

--- news/signals.py
+++ news/signals.py
@@ -1,20 +1,36 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+import threading
 
 from .models import News
 from .facebook import post_to_facebook
 
 
-@receiver(post_save, sender=News)
-def auto_post_news(sender, instance, created, **kwargs):
-
-    # if created:
+def delayed_facebook_post(news_id):
     try:
-        post_to_facebook(instance)
+        news = News.objects.get(id=news_id)
+
+        post_to_facebook(news)
+
         print("Facebook post successful")
-        instance.facebook_posted = True
-        News.objects.filter(pk=instance.pk).update(
+
+        News.objects.filter(
+            pk=news.pk
+        ).update(
             facebook_posted=True
         )
+
     except Exception as e:
         print("Facebook post failed:", e)
+
+
+@receiver(post_save, sender=News)
+def auto_post_news(sender, instance, created, **kwargs):
+    if created:
+        timer = threading.Timer(
+            5.0,
+            delayed_facebook_post,
+            args=[instance.id]
+        )
+
+        timer.start()
